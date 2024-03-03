a756e1066c2c75a533375cac7c33967dae224b5d
Danny <danny.goossen@gioxa.com>
2024-03-03 21:04:55 +0700

    add logging, systemd, config file dynmically add and remove TXT records

--- README.md
+++ README.md
@@ -17,28 +17,22 @@ Name:	192-168-10-236.sss.iot.v-odoo.com
 Address: 192.168.10.236
 ```
 
-with `sss`being the customer defined in the `dns-iot-customer.yaml`
-```yaml
-- psa
-- sss
-- tato
-```
 
-and with a base domain `iot.v-odoo.com` as defined in the `dns-config.yaml`:
+With a base domain `iot.v-odoo.com` as defined in the `dns-config.yaml` and subdomains as per subdomain array key:
 ```yaml
 host: 127.0.0.1
 port: 53
 base_domain: iot.v-odoo.com
+subdomains:
+  - psa
+  - sss
+  - tato
 ```
 
 ## TXT records
 
-TXT records are defned in `dns-iot-txt.yaml`
-```yaml
-_acmekey.sss: myacme text record data
-```
+test if present, see further under [Issuing Certificates](issuing_certificates) to add TXT records:
 
-test:
 ```bash
 $> nslookup -q=txt _acmekey.sss.iot.v-odoo.com 127.0.0.1
 Server:		127.0.0.1
@@ -52,21 +46,79 @@ _acmekey.sss.iot.v-odoo.com	text = "myacme" "text" "record" "data"
 in order for this to work, we need to define a `NS record` on the domain server.
 
 in our case: `v-odoo.com`is a cloud flare, so we add record:
-```
+```bash
 ìot.v-odoo.com NS 600 remote.v-odoo.com
 ``` 
-with `remote.v-odoo.com`pointing to our dns server.
+with `remote.v-odoo.com` pointing to our dns server.
 
-from that moment in time, dns queries for domain `iot.v-odoo.com` will arive at our new dns server.
+From that moment in time, dns queries for domain `iot.v-odoo.com` will arive at our new dns server.
 
-TODO:
-- use logger for messages
-- create systemd service
-- add command line options for config file location.
+
+#### usage stand alone: 
+```bash
+python3 dns-iot.py [-c myconfig/file.yaml]
+```
+
+### production usage
+
+config file in `/etc/dns-iot/dns-iot-config.yaml`
+
+#### Install
+```bash
+cp dns-iot.py /usr/bin/
+````
+
+#### enable and start
+
+```bash
+systemctl enable --now dns-iot.service
+```
+
+check if service is running
+```bash
+systemctl enable --now dns-iot.service
+```
+
+check logs with:
+```bash
+journalctl -u dns-iot.service
+```
 
 ## Issuing certificates
 
-update the `dns-iot-txt.yaml` with the challenge and restart our dns server.
+to issue a new certificate we need to add a specific TXT record to the server that letsencript can check to issue a certificate.
+Our dns server already can add and remove TXT records by use of the dns query TXT.
 
-### TODO:
- create an `certbot-dns-iot-vct` module that can update and restart/reload the dns server 
+### Add and remove TXT record
+
+**Note**: *Adding and removing records with the following method can only be done from the local server*
+
+Add a TXT record with the  **`:+:`** separator of key and value:
+```bash
+$>nslookup -q=txt "_acmekey.sss.iot.v-odoo.com:+:my_new_key" 127.0.0.1
+Server:		127.0.0.1
+Address:	127.0.0.1#53
+
+_acmekey.sss.iot.v-odoo.com	text = "my_new_key"
+```
+
+Query -> simulate lets encript:
+```bash
+nslookup -q=txt "_acmekey.sss.iot.v-odoo.com"
+Server:		127.0.0.1
+Address:	127.0.0.1#53
+
+_acmekey.sss.iot.v-odoo.com	text = "my_new_key"
+```
+
+Delete from the *local server* => use the **`:-:`** suffix on the txt query:
+```bash
+$>nslookup -q=txt "_acmekey.sss.iot.v-odoo.com:-:" 127.0.0.1
+Server:		127.0.0.1
+Address:	127.0.0.1#53
+
+_acmekey.sss.iot.v-odoo.com	text = ""
+```
+
+## TODO:
+ create a `certbot-dns-iot-vct` module that can update the dns server by using the **special** dns txt Query
---
+++ dns-iot.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python3
+import logging
+from optparse import OptionParser
+import yaml
+import ipaddress
+from socketserver import UDPServer, BaseRequestHandler
+from dnslib import DNSRecord, DNSHeader, RR, QTYPE
+
+global base_name
+global config
+global config_TXT
+global config_A
+
+config_TXT = {}
+config_A = {}
+config = {}
+
+base_domain = ""
+
+class DomainName(str):
+    def __getattr__(self, item):
+        return DomainName(item + "." + self)
+    
+class DNSHandler(BaseRequestHandler):
+    def handle(self):
+        data = self.request[0].strip()
+        request = DNSRecord.parse(data)
+
+        # print (request)
+        client, port = self.client_address
+
+        reply = DNSRecord(
+            DNSHeader(id=request.header.id, qr=1, aa=1, ra=1), q=request.q
+        )
+
+        qname = request.q.qname
+        qn = str(qname).lower()
+        qtype = request.q.qtype
+        qt = QTYPE[qtype]
+        #print (f'qt={qt}')
+        base_domain = "iot.v-odoo.com"
+        if qt == 'TXT' and ( ':-:' in qn or ':+:' in qn ) and base_domain in qn and client == '127.0.0.1':
+            # use TXT dns query to add or remove a TXT record from our dns server, 
+            # only possible from local 127.0.0.1 client
+            if ':+:' in qn:
+               split_qn=qn.split(':+:')
+               qn_clean=split_qn[0]
+               key=split_qn[0].split(f'.{base_domain}')[0]
+               value= split_qn[1][:-1]
+               config_TXT[key] = value 
+               reply.add_answer(*RR.fromZone(f"{qn_clean} 5 TXT {value}"))
+               print_value = (value[:15] + "..") if len(value) > 15 else value
+               logging.info(f"TXT KEY  ADDED  ->{key}: '{print_value}'")
+            else:
+                split_qn=qn.split(':-:')
+                key=split_qn[0].split(f'.{base_domain}')[0]
+                try:
+                    del config_TXT[key]
+                    reply.add_answer(*RR.fromZone(f"{split_qn[0]} 5 TXT ''"))
+                    logging.info(f" DNS TXT KEY REMOVED ->{key}")
+                except:
+                    reply = DNSRecord(
+                       DNSHeader( id=request.header.id, qr=1, aa=1, ra=1, rcode=5),
+                                  q=request.q 
+                                  )
+                    logging.error(f" KEY '{key}' to remove not found in list")
+        elif base_domain not in qn:
+            logging.error(f" DNS {qt}:{qn} from {client}:{port} wrong domain: REFUSED")
+            reply = DNSRecord(
+                DNSHeader(id=request.header.id, qr=1, aa=1, ra=1, rcode=5), q=request.q
+            )
+        elif f".{base_domain}." not in qn:
+            logging.error(
+                f"DNS {qt}:{qn} from {client}:{port} wrong sub domain format NXDOMAIN"
+            )
+            reply = DNSRecord(
+                DNSHeader(id=request.header.id, qr=1, aa=1, ra=1, rcode=3), q=request.q
+            )
+        elif qt == "A":
+            ip_address = None
+            found = False
+            for key in config_A:
+                if f".{key}.{base_domain}." in qn:
+                    # here happens the magic
+                    ip_address = qn.split(f".{key}.{base_domain}", 1)[0].replace(
+                        "-", "."
+                    )
+                    try:
+                        ip = ipaddress.ip_address(ip_address)
+                    except ValueError:
+                        pass
+                    else:
+                        found = True
+                        logging.info(f"DNS {qt}:{qn} from {client}:{port} -> {ip_address}")
+                        reply.add_answer(*RR.fromZone(f"{qn} 5 A {ip_address}"))
+            if not found:
+                logging.error(
+                    f"DNS {qt}:{qn} from {client}:{port} wrong sub domain format NXDOMAIN"
+                )
+                reply = DNSRecord(
+                    DNSHeader(id=request.header.id, qr=1, aa=1, ra=1, rcode=3),
+                    q=request.q,
+                )
+        elif qt == "TXT":
+            found = False
+            for key, value in config_TXT.items():
+                if qn == f"{key}.{base_domain}.":
+                    found = True
+                    print_value = (value[:15] + "..") if len(value) > 15 else value
+                    logging.info(f" DNS {qt}:{qn} from {client}:{port} -> {print_value}")
+                    reply.add_answer(*RR.fromZone(f"{qn} 5 TXT {value}"))
+            if not found:
+                reply = DNSRecord(
+                    DNSHeader(id=request.header.id, qr=1, aa=1, ra=1, rcode=3),
+                    q=request.q,
+                )
+                logging.error(f"DNS {qt}:{qn} from {client}:{port} wrong sub domain NXDOMAIN"
+                )
+        else:
+            logging.error(f" DNS {qt}:{qn} from {client}:{port} unsupported type")
+            # NOERROR
+        self.request[1].sendto(reply.pack(), self.client_address)
+
+
+if __name__ == "__main__":
+    config_file=""
+    parser = OptionParser()
+    parser.add_option("-c")
+    options, args = parser.parse_args()
+    config_file=options.c
+    if not config_file:
+       config_file="dns-iot-config.yaml" 
+    
+    try:
+        with open(config_file) as stream:
+            config = yaml.safe_load(stream)
+    except:
+        pass
+    try:
+        HOST = config["host"]
+    except:
+        HOST = "127.0.0.1"
+    try:
+        PORT = config["port"]
+    except:
+        PORT = 53
+    try:
+        base_domain = config["base_domain"]
+    except:
+        base_domain = "iot.v-odoo.com"
+
+    log_level='info'    
+    try:
+        log_level=config["log_level"]
+    except:
+        pass
+
+   # if log_level in "DEBUG":
+   #     logging.basicConfig(level=logging.DEBUG,format='%(module)s-%(funcName)s: %(message)s')
+   #     logging.debug("Debug level logging started")
+   # else:
+    logging.basicConfig(level=logging.INFO) #,format='%(module)s-%(funcName)s: %(message)s')
+    logging.info(f" -> DNS with config : {config_file}")
+    logging.info(f" -> DNS server serving on {HOST}:{PORT}")
+    logging.info(f" -> with DNS Base Domain: {base_domain}")
+
+    try:
+        config_A = config["subdomains"]
+    except:
+        pass
+
+    for key in config_A:
+        logging.info(f" --> DNS with subdomains for  A  records {key}")
+
+    with UDPServer((HOST, PORT), DNSHandler) as server:
+        logging.info(f"DNS server listening on {HOST}:{PORT}")
+        server.serve_forever()
