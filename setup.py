68473ebf25e861d530f83541a6aa43e38dab6f8b
Danny <danny.goossen@gioxa.com>
2024-04-07 21:58:57 +0700

    fix project linting

--- setup.py
+++ setup.py
@@ -1,58 +1,62 @@
-import os,sys
+import os, sys
 
 from setuptools import find_packages
 from setuptools import setup
-   
-version = '1.1.8'
+
+version = "1.1.8"
 
 install_requires = [
-    'setuptools>=41.6.0',
-    'wheel>=0.30',
-    'pyzmq>=25',
-    'zmq',
-    'pyyaml>=6',
-    'dnslib',
-    'ipaddress',
-    'pyjson>=1.4',
-    'urllib3==1.26.6;python_version>="3.8" and python_version<"3.11"', 
-    'requests',
-    'apscheduler==3.10.4'
+    "setuptools>=41.6.0",
+    "wheel>=0.30",
+    "pyzmq>=25",
+    "zmq",
+    "pyyaml>=6",
+    "dnslib",
+    "ipaddress",
+    "pyjson>=1.4",
+    'urllib3==1.26.6;python_version>="3.8" and python_version<"3.11"',
+    "requests",
+    "apscheduler==3.10.4",
 ]
 
 setup(
-    name='dns-iot-vct',
+    name="dns-iot-vct",
     version=version,
     description="DNS-IOT-VCT returns for a base domain and subdomain a parsed local ip",
-    url='https://github.com/v-odoo-testing/dns-iot-vct',
+    url="https://github.com/v-odoo-testing/dns-iot-vct",
     author="Danny Goossen",
-    author_email='danny@v-consulting.biz',
-    license='MIT',
-    python_requires='>=3.8',
+    author_email="danny@v-consulting.biz",
+    license="MIT",
+    python_requires=">=3.8",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Plugins',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Security',
-        'Topic :: System :: Installation/Setup',
-        'Topic :: System :: Networking',
-        'Topic :: System :: Systems Administration',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Plugins",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Security",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: System :: Networking",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
     ],
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
-    scripts=['dns_iot/dns_iot.py'],
-    data_files=[("dns-iot-vct-post-install",['etc/systemd/system/dns-iot.service','etc/dns-iot/dns-iot-config.yaml'])],
-    test_suite='tests'
-
+    scripts=["dns_iot/dns_iot.py"],
+    data_files=[
+        (
+            "dns-iot-vct-post-install",
+            ["etc/systemd/system/dns-iot.service", "etc/dns-iot/dns-iot-config.yaml"],
+        )
+    ],
+    test_suite="tests",
 )
--- test_dns/dns_test.py
+++ test_dns/dns_test.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
-import logging 
+import logging
 import zmq
 import time
 
 logger = logging.getLogger(__name__)
 
+
 def add_txt_record(record_name: str, record_content: str):
 
     context = zmq.Context()
-    context.RCVTIMEO=1000
-    context.SNDTIMEO=1000
-    answer=''
-    request=f'_TXT_:{record_name}:+:{record_content}'
+    context.RCVTIMEO = 1000
+    context.SNDTIMEO = 1000
+    answer = ""
+    request = f"_TXT_:{record_name}:+:{record_content}"
     #  Socket to talk to server
     print("Connecting to hello world server…")
     socket = context.socket(zmq.REQ)
@@ -20,129 +21,131 @@ def add_txt_record(record_name: str, record_content: str):
     try:
         socket.send(request.encode())
     except Exception as e:
-        logger.error('Encountered error adding TXT record: {0}'
-                                     .format(e))
-    
+        logger.error("Encountered error adding TXT record: {0}".format(e))
+
     try:
         answer = socket.recv()
     except Exception as e:
-        logger.error('Encountered error adding TXT record, receive reply: {0}'
-                                     .format(e))
+        logger.error(
+            "Encountered error adding TXT record, receive reply: {0}".format(e)
+        )
     else:
         answer = answer.decode("utf-8")
-        if answer == 'OK':
-            logger.info('Successfully added TXT  %s %s', record_name,record_content)
+        if answer == "OK":
+            logger.info("Successfully added TXT  %s %s", record_name, record_content)
         else:
-            logger.error('Encountered error add TXT, bad reply: {0}'
-                                        .format(answer)) 
+            logger.error("Encountered error add TXT, bad reply: {0}".format(answer))
     socket.close()
+
 
 def add_a_record(record_name: str):
     context = zmq.Context()
-    context.RCVTIMEO=1000
-    context.SNDTIMEO=1000
-    answer=''
-    request=f'__A__:{record_name}:+:'
+    context.RCVTIMEO = 1000
+    context.SNDTIMEO = 1000
+    answer = ""
+    request = f"__A__:{record_name}:+:"
     #  Socket to talk to server
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://127.0.0.1:5555")
     try:
         socket.send(request.encode())
     except Exception as e:
-        logger.error('Encountered error adding SUBDOMAIN record: {0}'
-                                     .format(e))
+        logger.error("Encountered error adding SUBDOMAIN record: {0}".format(e))
     try:
         answer = socket.recv()
     except Exception as e:
-        logger.error('Encountered error adding TXT record, receive reply: {0}'
-                                     .format(e))
+        logger.error(
+            "Encountered error adding TXT record, receive reply: {0}".format(e)
+        )
     else:
         answer = answer.decode("utf-8")
-        if answer == 'OK':
-            logger.info('Successfully added SUBDOMAIN  %s', record_name)
+        if answer == "OK":
+            logger.info("Successfully added SUBDOMAIN  %s", record_name)
         else:
-            logger.error('Encountered error add DOMAIN %s, bad reply: {0}'
-                                         .format(answer)) 
+            logger.error(
+                "Encountered error add DOMAIN %s, bad reply: {0}".format(answer)
+            )
     socket.close()
+
 
 def del_txt_record(record_name: str, record_content: str):
     context = zmq.Context()
-    answer=''
-    request = f'_TXT_:{record_name}:-:{ record_content}'
-    context.RCVTIMEO=1000
-    context.SNDTIMEO=1000
+    answer = ""
+    request = f"_TXT_:{record_name}:-:{ record_content}"
+    context.RCVTIMEO = 1000
+    context.SNDTIMEO = 1000
     socket = context.socket(zmq.REQ)
     try:
         socket.connect("tcp://127.0.0.1:5555")
     except socket.error as msg:
         print("Socket Error: %s" % msg)
     except TypeError as msg:
-        print( "Type Error: %s" % msg)
-
+        print("Type Error: %s" % msg)
 
     try:
-        socket.send( request.encode("utf-8"))
+        socket.send(request.encode("utf-8"))
     except Exception as e:
-        logger.error('Encountered error adding TXT record, send request: {0}'
-                                     .format(e))
+        logger.error("Encountered error adding TXT record, send request: {0}".format(e))
     try:
         answer = socket.recv()
     except Exception as e:
-        logger.error('Encountered error adding TXT record, receive reply: {0}'
-                                     .format(e))
+        logger.error(
+            "Encountered error adding TXT record, receive reply: {0}".format(e)
+        )
     else:
         answer = answer.decode("utf-8")
         if "OK" == answer:
-            logger.info('Successfully removed record %s TXT %s', record_name,answer)
+            logger.info("Successfully removed record %s TXT %s", record_name, answer)
         else:
-            logger.error('Encountered error remove TXT record, bad reply: {0}'
-                                     .format(answer)) 
+            logger.error(
+                "Encountered error remove TXT record, bad reply: {0}".format(answer)
+            )
     socket.close()
+
 
 def del_a_record(record_name: str):
     context = zmq.Context()
-    answer=''
-    request = f'__A__:{record_name}:-:'
-    context.RCVTIMEO=1000
-    context.SNDTIMEO=1000
+    answer = ""
+    request = f"__A__:{record_name}:-:"
+    context.RCVTIMEO = 1000
+    context.SNDTIMEO = 1000
     socket = context.socket(zmq.REQ)
 
     socket.connect("tcp://127.0.0.1:5555")
 
     try:
-        socket.send( request.encode("utf-8"))
+        socket.send(request.encode("utf-8"))
     except Exception as e:
-        logger.error('Encountered error remove subdomain, send request: {0}'
-                                     .format(e))
+        logger.error("Encountered error remove subdomain, send request: {0}".format(e))
     try:
         answer = socket.recv()
     except Exception as e:
-        logger.error('Encountered error remove subdomain, receive reply: {0}'
-                                     .format(e))
+        logger.error("Encountered error remove subdomain, receive reply: {0}".format(e))
     else:
         answer = answer.decode("utf-8")
         if "OK" == answer:
-            logger.info('Successfully removed domain(s) %s', record_name)
+            logger.info("Successfully removed domain(s) %s", record_name)
         else:
-            logger.error('Encountered error remove subdomains, bad reply: {0}'
-                                     .format(answer)) 
+            logger.error(
+                "Encountered error remove subdomains, bad reply: {0}".format(answer)
+            )
     socket.close()
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    add_txt_record('_acmechallenge.psa.iotdns.v-odoo.com','TRW4HC1Wtzhr6Z9DiEMpqVOMu87QUgYaCeEcFoIgLHQ')
-    #time.sleep(1)
-    #del_txt_record('_acmechallenge.psa.iotdns.v-odoo.com','') #'TRW4HC1Wtzhr6Z9DiEMpqVOMu87QUgYaCeEcFoIgLHQ')
-    #add_a_record('danny.iotdns.v-odoo.com')
-    #add_a_record('sss.iotdns.v-odoo.com')
-    #add_a_record('psa.iotdns.v-odoo.com')
-    #add_a_record('gvs.iotdns.v-odoo.com')
-    #add_a_record('gvs.iot.v-odoo.com')
-    #del_a_record('psa.iotdns.v-odoo.com')
-    #del_a_record('.iotdns.v-odoo.com')
-    #del_a_record('*.dns.v-odoo.com')
-    #del_a_record('*.iotdns.v-odoo.com')
-    
-
-
+    add_txt_record(
+        "_acmechallenge.psa.iotdns.v-odoo.com",
+        "TRW4HC1Wtzhr6Z9DiEMpqVOMu87QUgYaCeEcFoIgLHQ",
+    )
+    # time.sleep(1)
+    # del_txt_record('_acmechallenge.psa.iotdns.v-odoo.com','') #'TRW4HC1Wtzhr6Z9DiEMpqVOMu87QUgYaCeEcFoIgLHQ')
+    # add_a_record('danny.iotdns.v-odoo.com')
+    # add_a_record('sss.iotdns.v-odoo.com')
+    # add_a_record('psa.iotdns.v-odoo.com')
+    # add_a_record('gvs.iotdns.v-odoo.com')
+    # add_a_record('gvs.iot.v-odoo.com')
+    # del_a_record('psa.iotdns.v-odoo.com')
+    # del_a_record('.iotdns.v-odoo.com')
+    # del_a_record('*.dns.v-odoo.com')
+    # del_a_record('*.iotdns.v-odoo.com')
